e3a2da520437766f689e3be914d0fb1f5098f9fb
ivanbearzotti <ivandanilobearzotti@gmail.com>
2023-06-23 18:18:01 -0300

    Se modifcan logica de carrito

--- public/js/products.js
+++ public/js/products.js
@@ -33,23 +33,23 @@ let carrito = [];
 
 let contenedorProductos = document.getElementById("productos");
 
+let cartCards = document.getElementById("cart-cards");
+
 productos.forEach(producto => {
     let cardContainer = document.createElement("div");
     contenedorProductos.append(cardContainer);
-    cardContainer.id = "card-container";
-    cardContainer.className = "bg-white rounded-xl flex flex-col p-8 gap-4 drop-shadow-xl lg:transition lg:duration-300 lg:hover:scale-105";
+    cardContainer.className = "card-container bg-white rounded-xl flex flex-col p-8 gap-4 drop-shadow-xl lg:transition lg:duration-300 lg:hover:scale-105";
 
     let cardImg = document.createElement("div");
     cardContainer.append(cardImg);
-    cardImg.id = "card-img";
+    cardImg.className = "card-img";
     cardImg.innerHTML = `
         <img src="${producto.img}" alt="${producto.name}" class="flex justify-center w-full lg:transition lg:duration-300 lg:hover:scale-105">
     `;
 
     let cardInfo = document.createElement("div");
     cardContainer.append(cardInfo);
-    cardInfo.id = "card-info";
-    cardInfo.className = "flex flex-col gap-1";
+    cardInfo.className = "card-info flex flex-col gap-1";
     cardInfo.innerHTML = `
         <div class="name-description-price flex justify-between">
             <div class="name-description">
@@ -64,8 +64,7 @@ productos.forEach(producto => {
 
     let botonAgregar = document.createElement("button");
     cardContainer.append(botonAgregar);
-    botonAgregar.id = "boton-agregar";
-    botonAgregar.className = "font-poppins font-bold text-white text-m bg-gris w-full px-4 py-2 rounded-lg";
+    botonAgregar.className = "boton-agregar font-poppins font-bold text-white text-m bg-gris w-full px-4 py-2 rounded-lg";
     botonAgregar.innerText = "Add to cart";
 
     botonAgregar.addEventListener("click", () => {
@@ -77,10 +76,46 @@ productos.forEach(producto => {
             stock: producto.stock,
             img: producto.img}
         );
+        carrito.forEach(productoCarro => {
+            let cartCard = document.createElement("div");
+            cartCards.append(cartCard);
+            cartCard.className = "cart-card flex gap-4 justify-between bg-white p-4 mt-4 rounded-xl drop-shadow-xl";
+            cartCard.innerHTML = `
+                <div class="cart-card-img flex items-center">
+                    <img src="${productoCarro.img}" alt="${productoCarro.name}" class="h-32">
+                </div>
+                <div class="card-card-info flex flex-col gap-2">
+                    <div class="name-type-delete flex gap-10">
+                        <div class="name-type flex flex-col">
+                            <h3 class="font-poppins font-bold">${productoCarro.name}</h3>
+                            <p class="font-poppins font-normal text-gris text-sm">${productoCarro.type}</p>
+                        </div>
+                        <button class="bg-gray-200 py-1 px-2 rounded"><i class='bx bx-trash text-2xl text-gris'></i></button>
+                    </div>
+                    <div class="cont-cant flex bg-gray-200 rounded-lg justify-between items-center px-1 py-1 w-24">
+                        <span class="menos font-poppins font-bold text-gris text-md border-r border-solid border-gris px-2 cursor-pointer">-</span>
+                        <span class="num font-poppins font-bold text-gris text-md">1</span>
+                        <span class="mas font-poppins font-bold text-gris text-md border-l border-solid border-gris px-2 cursor-pointer">+</span>
+                    </div>
+                    <div class="unit-price-total">
+                        <div class="unit-price flex items-center justify-between">
+                            <p class="unit-p font-poppins font-normal text-gris text-sm">Unit price:</p>
+                            <p class="price font-poppins font-bold">$${productoCarro.price}</p>
+                        </div>
+                        <div class="total-price flex items-center justify-between">
+                            <p class="total-p font-poppins font-normal text-gris text-sm">Total:</p>
+                            <p class="price font-poppins font-bold">$${productoCarro.price}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        })
     });
 });
 
 // CARRITO DE COMPRAS
+
+
 
 
 
